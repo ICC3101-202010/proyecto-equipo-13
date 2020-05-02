23f32f2b3598272c7bb7dc454de558f35cb2888b
RobertoVergaraC <ravergara@miuandes.com>
2020-05-02 12:20:35 -0400

    Keko2 clase user lista

--- ProyectoEquipo13/ProyectoEquipo13/User.cs
+++ ProyectoEquipo13/ProyectoEquipo13/User.cs
@@ -9,16 +9,30 @@ namespace ProyectoEquipo13
     public class User
     {
         protected int UserID;
-        protected string user
+        protected string user;
         protected string Email;
         protected string Password;
         protected bool Privacy;
 
-        public bool CreateAccount(Files.AllUsers() list, string email, string password,)
+        public bool CreateAccount(Files.AllUsers() list, string email, string password, int userid)
         {
             for (int i = 0; i <list.Count ; i++)
             {
-                if list[i].Email==email && list[i].Password==password || list[i].user==user
+                if ((list[i].Email==email ) || (list[i].UserID == userid))
+                {
+                    Console.WriteLine("La cuenta no se puede crear, debido a que ya existe, o el mail usado ya esta usado");
+                    return false;
+                }
+                else if (list[i].Email != email || list[i].UserID != userid)
+                {
+                    Console.WriteLine("La cuenta ha sido creada correctamente");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("La opción que seleccionó no es válida");
+                    return false;
+                }
             }
         }
     }
