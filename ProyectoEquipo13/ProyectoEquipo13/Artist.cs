09cdf922ba21037e15b087b35960cd1bc78216c9
RobertoVergaraC <ravergara@miuandes.com>
2020-05-02 12:13:22 -0400

    keko

--- ProyectoEquipo13/ProyectoEquipo13/Artist.cs
+++ ProyectoEquipo13/ProyectoEquipo13/Artist.cs
@@ -8,5 +8,6 @@ namespace ProyectoEquipo13
 {
     class Artist
     {
+    
     }
 }
--- ProyectoEquipo13/ProyectoEquipo13/User.cs
+++ ProyectoEquipo13/ProyectoEquipo13/User.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProyectoEquipo13
 {
-    public abstract class User
+    public class User
     {
+        protected int UserID;
+        protected string user
+        protected string Email;
+        protected string Password;
+        protected bool Privacy;
 
+        public bool CreateAccount(Files.AllUsers() list, string email, string password,)
+        {
+            for (int i = 0; i <list.Count ; i++)
+            {
+                if list[i].Email==email && list[i].Password==password || list[i].user==user
+            }
+        }
     }
 }
